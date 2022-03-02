afc84ae19c932156770f80181b4bd02ef94dc798
User <elyorshovkatov98@gmail.com>
2022-03-02 19:01:51 +0500

    modal

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,14 +5,19 @@ import {CircularProgress} from "@mui/material";
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 import Box from '@mui/material/Box';
+import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import {Map, Placemark, YMaps} from "react-yandex-maps";
 
 export const Home = () => {
     const state = useSelector((state) => state.users);
     const newStates = useSelector((state) => state.searched);
     const loading = useSelector((state) => state.loading);
-    const [modalId,setModalId]=useState(null)
+    const [modalId, setModalId] = useState(null)
     const dispatch = useDispatch();
-    const modalData = state.filter(i=>i.id===modalId)
+    const modalData = state.filter(i => i.id === modalId)
 
     useEffect(() => {
         if (state.length === 0) {
@@ -20,13 +25,12 @@ export const Home = () => {
         }
     }, []);
 
-    const cheking=()=>{
+    const cheking = () => {
         let news = null
-        if (newStates.length===0){
-            news=state
-        }
-        else if(newStates.length!==0){
-            news=newStates
+        if (newStates.length === 0) {
+            news = state
+        } else if (newStates.length !== 0) {
+            news = newStates
         }
         return news
     }
@@ -43,12 +47,17 @@ export const Home = () => {
 
     return loading ? (
         <div className="flex justify-center items-center h-screen">
-            <CircularProgress color="success" size={150} />
+            <CircularProgress color="success" size={150}/>
         </div>
-    ) : (<div><div className="flex w-full justify-center"><h2 className=" px-3 bg-slate-200 inline-block text-2xl font-bold uppercase translate-y-4">Все мероприятие</h2></div><hr className="h-0.5 bg-yellow-400"/>
+    ) : (<div>
+        <div className="flex w-full justify-center"><h2
+            className=" px-3 bg-slate-200 inline-block text-2xl font-bold uppercase translate-y-4">Все мероприятие</h2>
+        </div>
+        <hr className="h-0.5 bg-yellow-400"/>
         <div className="card_wrapper mt-4 pt-6 flex flex-wrap justify-between">
             {cheking().map((i) => (
-                <div key={i.id} onClick={()=>handleOpen(i.id)} className="hover:scale-110 duration-700 w-11/12 sm:w-80 md:w-96 xl:w-80 mx-auto mb-14 bg-white rounded-md overflow-hidden">
+                <div key={i.id} onClick={() => handleOpen(i.id)}
+                     className="hover:scale-110 duration-700 w-11/12 sm:w-80 md:w-96 xl:w-80 mx-auto mb-14 bg-white rounded-md overflow-hidden">
                     <img
                         className="w-full h-72 "
                         src={`http://f0607823.xsph.ru/elyor/public/storage/event/${i.id}/${i.id}.jpg`}
@@ -70,15 +79,41 @@ export const Home = () => {
             aria-describedby="modal-modal-description"
         >
             <>
-                {modalId?<Box className="modal">
+                {modalId ? <Box className="modal">
                     <Typography id="modal-modal-title" variant="h6" component="h2">
                         {modalData[0].title}
                     </Typography>
                     <Typography id="modal-modal-description">
                         {modalData[0].info}
                     </Typography>
-                    <div></div>
-                </Box>:''}
+                    <div className="flex h-72">
+                        <img
+                            className="w-1/3"
+                            src={`http://f0607823.xsph.ru/elyor/public/storage/event/${modalData[0].id}/${modalData[0].id}.jpg`}
+                            alt={modalData[0].title}
+                        />
+                        <div>
+                            <span className="text-xl flex items-center mb-2"><CalendarTodayIcon className="mr-1"/>{modalData[0].date}</span>
+                            <span className="text-xl flex items-center mb-2"><LocationOnIcon className="mr-1"/>{modalData[0].address}</span>
+                            <span className="text-xl flex items-center mb-2"><AttachMoneyIcon className="mr-1"/>{modalData[0].price} сум</span>
+                            <span className="text-xl flex items-center mb-2"><LocalPhoneIcon className="mr-1"/>{modalData[0].contact}</span>
+                        </div>
+                        <div className="w-1/3 h-72">
+                            <YMaps>
+                                <Map
+                                    defaultState={{
+                                        center: [(+modalData[0].location.split(',')[0]), (+modalData[0].location.split(',')[1])],
+                                        zoom: 12,
+                                    }}
+                                    width="auto"
+                                    height="288px"
+                                >
+                                    <Placemark geometry={[(+modalData[0].location.split(',')[0]), (+modalData[0].location.split(',')[1])]} />
+                                </Map>
+                            </YMaps>
+                        </div>
+                    </div>
+                </Box> : ''}
             </>
         </Modal>
     </div>)
